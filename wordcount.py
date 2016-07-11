f2b0e79a7d357ee7aedf9f79776b2ca645651b3d
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 13:02:31 -0700

    Better for loops and function returns dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,25 @@
 # put your code here.
 
 def count_words(path):
+   
     text_file = open(path)
     word_count_dict = {}
+    words = []
+
     for line in text_file:
         line = line.rstrip()
-        words = line.split(" ")
-        for word in words:
-            word_count_dict[word] = word_count_dict.get(word, 0) + 1
+        words.extend(line.split(" "))
 
-    for word, word_count in word_count_dict.items():
-        print "%s %d" % (word, word_count)
+    for word in words:
+        word_count_dict[word] = word_count_dict.get(word, 0) + 1
+
+    # for word, word_count in word_count_dict.iteritems():
+    #     print "%s %d" % (word, word_count)
     
     text_file.close()
+
+    return word_count_dict
+
 
 
 
